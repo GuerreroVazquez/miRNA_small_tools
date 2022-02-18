8a0cc522732119932e25d3f857bd3767e7f08569
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2022-02-18 10:12:29 +0000

    MML49_Divide_columns

---
+++ paper_helper/column_divider.py
@@ -0,0 +1,5 @@
+###
+# This file will contain the classes to distinguish the different columns of the
+# databases. For this I will annotate them on a header(?) maybe, maybe a dictionary
+# with the equivalences
+###
