1846d8b31519e83347e9935f80929a32836b7864
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2022-02-21 19:02:41 +0000

    MML49_staringSQL_connection

---
+++ paper_helper/sql_operations.py
@@ -0,0 +1,24 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+config = {
+  'user': 'scott',
+  'password': 'password',
+  'host': '127.0.0.1',
+  'database': 'employees',
+  'raise_on_warnings': True
+}
+
+def connect_sql():
+    try:
+        cnx = mysql.connector.connect(user='scott',
+                                      database='employ')
+        return cnx
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        return None
