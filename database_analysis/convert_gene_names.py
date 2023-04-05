052b0694c0d59c4f3eb97583409f144633513ac6
GuerreroVazquez <GuerreroVazquez.Karen@gmail.com>
2023-04-05 07:56:45 +0100

    Update convert_gene_names.py

--- database_analysis/convert_gene_names.py
+++ database_analysis/convert_gene_names.py
@@ -29,22 +29,29 @@ def get_genes_from_transcripts(provitional_gene):
     :param provitional_gene:
     :return: dict
     """
-    ids = ncbi_connection.fetch_queries_ids(term=provitional_gene)
-    if len(ids) < 1:
-        logger.error(f"The gene {provitional_gene} couldn't be found in NCBI")
-        return None
-    #for id in ids:
-    #id = id[0]
-    results = ncbi_connection.get_ids_information(db_id=ids)
     dict_transcript_gene = {}
-    for result in results:
-        locus = result.locus
-        if locus in provitional_gene:
-            gene = result.gene
-            if gene is None:
-                logger.warning(f"There was no gene name for {provitional_gene} with Id {ids}")
-            dict_transcript_gene[locus]=gene
-            logger.info(f"The name for {provitional_gene} will be {gene}")
+    try:
+        ids = ncbi_connection.fetch_queries_ids(term=provitional_gene)
+        if len(ids) < 1:
+            logger.error(f"The gene {provitional_gene} couldn't be found in NCBI")
+            return None
+        #for id in ids:
+        #id = id[0]
+        results = ncbi_connection.get_ids_information(db_id=ids)
+        for result in results:
+            locus = result.locus
+            if locus in provitional_gene:
+                gene = result.gene
+                if gene is None:
+                    logger.warning(f"There was no gene name for {provitional_gene} with Id {ids}")
+                dict_transcript_gene[locus]=gene
+                logger.info(f"The name for {provitional_gene} will be {gene}")
+    except ValueError as ve:
+        print(ve)
+    except ConnectionError as ce:
+        print(ce)
+    except Exception as ex:
+        print(ex)
     return dict_transcript_gene
 
 def get_update_queries(genes, n=100, is_test=False):
@@ -102,8 +109,8 @@ def convert_all_genes():
     genes = list(set(genes))
 
     update_query = get_update_queries(genes, n=250)
-    failed_updates = update_queries(update_query)
-
+    # failed_updates = update_queries(update_query)
+    failed_updates = len (update_query)
             # query = f"Select * from binding where mrna = '{real_name}' limit 10"
             # nuevo = sql.run_query(query=query)
     return failed_updates
@@ -115,5 +122,6 @@ def modify_gene():
 
 
 if __name__ == '__main__':
-    convert_all_genes()
+    n = convert_all_genes()
+    print (n)
     pass
